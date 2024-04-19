4604d3d4467ba98eee4f4d559118232a46c51380
MuallaShn <muallashn11@gmail.com>
2024-04-19 14:17:17 +0300

    new codes

--- src/App.jsx
+++ src/App.jsx
@@ -5,45 +5,42 @@ import './App.css';
 
 
 function App() {
-  const[todos,setTodos]=useState([]);
-  
+  const [todos, setTodos] = useState([]);
 
-  const createTodo=(newTodo)=>{
-    setTodos([...todos,newTodo]);
-    
+
+  const createTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+
   }
-  const removeTodo=(todoId)=>{
-    setTodos([...todos.filter((todo)=> todo.id!==todoId)]);
+  const removeTodo = (todoId) => {
+    setTodos([...todos.filter((todo) => todo.id !== todoId)]);
   }
 
-  const updateTodo=(newTodo)=>{
-    const updatedTodos =todos.map((todo)=>{
-    if(todo.id!==newTodo.id){
-    return todo;
-    }
-    return newTodo;
-  })
+  const updateTodo = (newTodo) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id !== newTodo.id) {
+        return todo;
+      }
+      return newTodo;
+    })
 
-  setTodos([...updatedTodos]);
+    setTodos([...updatedTodos]);
   }
   console.log(todos);
 
   return (
-    
-
-    
     <div className='App' >
       <div className='main' >
-      <h1 style={{textAlign: 'center',}}>Todo List</h1>
+        <h1 style={{ textAlign: 'center', }}>Todo List</h1>
         <TodoCreate onCreateTodo={createTodo} />
-        <TodoList todos={todos} onRemoveTodo={removeTodo} onUpdateTodo={updateTodo}/>
-       
+        <TodoList todos={todos} onRemoveTodo={removeTodo} onUpdateTodo={updateTodo} />
+
       </div>
-      
+
 
     </div>
-    
-    
+
+
   )
 }
 
